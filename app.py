b38afa687c55238851e17b0d491a0fc07bbdfd8f
Santino <marzanosantino@gmail.com>
2026-03-06 10:42:01 -0300

    cambios

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ def index():
     # Datos de contacto que se pasan a la plantilla HTML
     contact_data = {
         "whatsapp_link": "https://wa.me/5492645551234",
-        "email": "info@aurapilates.com.ar",
+        "email": "clubpilatesanjuan@gmail.com",
         "address": "San Roque Sur 1044, Rawson, San Juan",
         # La clave de API de Google Maps se debe gestionar de forma segura
         "google_maps_api_key": "TU_API_KEY_AQUI" 
